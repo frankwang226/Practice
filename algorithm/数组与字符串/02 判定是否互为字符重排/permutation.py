426771659021ff251de39aefaa4d6f73ecdcfe2d
frankwang226 <frankwang910226@gmail.com>
2024-02-27 11:54:46 +0800

    判定是否互为字符重排，添加解法2

--- algorithm/数组与字符串/02 判定是否互为字符重排/permutation.py
+++ algorithm/数组与字符串/02 判定是否互为字符重排/permutation.py
@@ -17,7 +17,7 @@
 
 
 class Solution:
-    def CheckPermutation(self, s1: str, s2: str) -> bool:
+    def CheckPermutation1(self, s1: str, s2: str) -> bool:
         for char in s1:
             if char not in s2:
                 return False
@@ -28,12 +28,28 @@ class Solution:
         else:
             return False
 
+    def CheckPermutation2(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        s1_list = [char for char in s1]
+        s2_list = [char for char in s2]
+        s1_list.sort()
+        s2_list.sort()
+        if s1_list == s2_list:
+            return True
+        else:
+            return False
+
 
 def test_CheckPermutation():
     permutation = Solution()
-    assert True == permutation.CheckPermutation("abc", "bca")
-    assert False == permutation.CheckPermutation("abc", "bad")
-    assert False == permutation.CheckPermutation("abb", "aab")
+    assert True == permutation.CheckPermutation1("abc", "bca")
+    assert False == permutation.CheckPermutation1("abc", "bad")
+    assert False == permutation.CheckPermutation1("abb", "aab")
+
+    assert True == permutation.CheckPermutation2("abc", "bca")
+    assert False == permutation.CheckPermutation2("abc", "bad")
+    assert False == permutation.CheckPermutation2("abb", "aab")
 
 
 if __name__ == '__main__':
